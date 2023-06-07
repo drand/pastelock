edf23af637e689d18139b1288b77c90b6c1ada44
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-06-07 13:56:25 +0200

    moved dev dependencies to full deps

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,7 +7,7 @@ export type Config = {
 
 export function createConfig(): Config {
     return {
-        dbURL: envOrExplode("DB_URL"),
+        dbURL: envOrExplode("DATABASE_URL"),
         dbPort: Number.parseInt(envOrExplode("DB_PORT"), 10),
         dbUsername: envOrExplode("DB_USERNAME"),
         dbPassword: envOrExplode("DB_PASSWORD"),
