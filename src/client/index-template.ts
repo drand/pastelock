0369552d5b53adf706d8552fb24d7c6fdac8fd78
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-06-15 11:38:36 +0200

    updated index template

--- src/client/index-template.ts
+++ src/client/index-template.ts
@@ -4,9 +4,9 @@ export const indexTemplate = (html: string, css: string) => `
 <head>
     <meta charset="utf-8"/>
     <title>pastelock</title>
-    <link rel="stylesheet" href="./public/styles.css"/>
+    <link rel="stylesheet" href="/public/styles.css"/>
     ${css}
-    <script async type="text/javascript" src="./public/app.js"></script>
+    <script async type="text/javascript" src="/public/app.js"></script>
 </head>
 <body>
 <div id="root">${html}</div>
